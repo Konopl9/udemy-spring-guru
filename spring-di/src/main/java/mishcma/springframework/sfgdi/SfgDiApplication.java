ef949c74d9b7c7d0f678631970146c37f5d73bfc
mishcma <maksym.mishchenko@storaenso.com>
2022-05-31 21:35:57 +0200

    di-2 DI with spring framework

--- spring-di/src/main/java/mishcma/springframework/sfgdi/SfgDiApplication.java
+++ spring-di/src/main/java/mishcma/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package mishcma.springframework.sfgdi;
 
+import mishcma.springframework.sfgdi.controllers.ConstructorInjectedController;
 import mishcma.springframework.sfgdi.controllers.MyController;
+import mishcma.springframework.sfgdi.controllers.PropertyInjectedController;
+import mishcma.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +20,25 @@ public class SfgDiApplication {
         String greeting = myController.sayHello();
 
         System.out.println(greeting);
+
+        System.out.println("----- Property");
+
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("----- Setter");
+
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+        System.out.println(setterInjectedController.getGreeting());
+
+        System.out.println("----- Constructor ");
+
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+        System.out.println(constructorInjectedController.getGreeting());
+
     }
 
 }
--- spring-di/src/main/java/mishcma/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ spring-di/src/main/java/mishcma/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package mishcma.springframework.sfgdi.controllers;
 
 import mishcma.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
