be0ecbc38a41a2b381853e57d617c9a65189fe53
mishcma <maksym.mishchenko@storaenso.com>
2022-05-31 21:59:37 +0200

    di-4 primary bean

---
+++ spring-di/src/main/java/mishcma/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,14 @@
+package mishcma.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Controller;
+
+@Primary
+@Controller
+public class PrimaryGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the PRIMARY Bean";
+    }
+}
