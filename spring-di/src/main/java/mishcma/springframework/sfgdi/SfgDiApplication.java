2cba074beeea3a8ca233a77f3b3567a123f26651
Konopl9 <maksimus2998@gmail.com>
2022-06-05 10:25:39 +0200

    di + factory patter

--- spring-di/src/main/java/mishcma/springframework/sfgdi/SfgDiApplication.java
+++ spring-di/src/main/java/mishcma/springframework/sfgdi/SfgDiApplication.java
@@ -1,21 +1,19 @@
 package mishcma.springframework.sfgdi;
 
-import mishcma.springframework.sfgdi.controllers.ConstructorInjectedController;
-import mishcma.springframework.sfgdi.controllers.I18nController;
-import mishcma.springframework.sfgdi.controllers.MyController;
-import mishcma.springframework.sfgdi.controllers.PropertyInjectedController;
-import mishcma.springframework.sfgdi.controllers.SetterInjectedController;
+import mishcma.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"mishcma.springframework.sfgdi", "com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        PetController petController = ctx.getBean("petController", PetController.class);
+        System.out.println("--- The Best Pet is ---");
+        System.out.println(petController.whichPetIsTheBest());
 
         I18nController i18Controller = (I18nController) ctx.getBean("i18nController");
         System.out.println("----- Profile Bean");
