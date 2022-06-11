51ba5d2230e07819c0d113331a5d6122a5a68f52
mishcma <maksym.mishchenko@storaenso.com>
2022-06-11 16:03:54 +0200

    rp-7 added unit of measure to db

--- recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/IndexController.java
+++ recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package mishcma.springweb.recipeproject.controllers;
 
+import java.util.Optional;
+import mishcma.springweb.recipeproject.domain.Category;
+import mishcma.springweb.recipeproject.domain.UnitOfMeasure;
+import mishcma.springweb.recipeproject.repositories.CategoryRepository;
+import mishcma.springweb.recipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(
+        CategoryRepository categoryRepository,
+        UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasure.get().getId());
         return "index";
     }
 }
--- recipe-project/src/main/java/mishcma/springweb/recipeproject/repositories/UnitOfMeasureRepository.java
+++ recipe-project/src/main/java/mishcma/springweb/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -1,8 +1,10 @@
 package mishcma.springweb.recipeproject.repositories;
 
+import java.util.Optional;
 import mishcma.springweb.recipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
