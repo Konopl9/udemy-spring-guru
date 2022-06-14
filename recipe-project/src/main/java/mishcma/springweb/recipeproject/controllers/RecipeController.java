323097c2b10196feda108f7f1c54ed72a615926a
mishcma <maksym.mishchenko@storaenso.com>
2022-06-15 00:28:27 +0200

    rp-16 added create recipe functionality to the controller

--- recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/RecipeController.java
+++ recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/RecipeController.java
@@ -1,10 +1,14 @@
 package mishcma.springweb.recipeproject.controllers;
 
+import mishcma.springweb.recipeproject.commands.RecipeCommand;
 import mishcma.springweb.recipeproject.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RecipeController {
@@ -20,5 +24,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
