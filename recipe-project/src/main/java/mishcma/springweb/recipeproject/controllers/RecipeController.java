d43f6cb5b5bb8bc3b537dbc4a0406f1e4edcd5c6
mishcma <maksym.mishchenko@storaenso.com>
2022-06-15 01:00:27 +0200

    rp-17 added update recipe functionality to the controller

--- recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/RecipeController.java
+++ recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/RecipeController.java
@@ -19,7 +19,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
@@ -39,5 +39,12 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/recipeform";
     }
 }
