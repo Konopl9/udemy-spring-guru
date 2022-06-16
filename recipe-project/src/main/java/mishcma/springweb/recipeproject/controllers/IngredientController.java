1cebec964461b9fadd4d51670ef92d2f559e5e8c
mishcma <maksym.mishchenko@storaenso.com>
2022-06-17 00:01:54 +0200

    rp-19 added add and delete for ingredients

--- recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/IngredientController.java
+++ recipe-project/src/main/java/mishcma/springweb/recipeproject/controllers/IngredientController.java
@@ -2,6 +2,8 @@ package mishcma.springweb.recipeproject.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import mishcma.springweb.recipeproject.commands.IngredientCommand;
+import mishcma.springweb.recipeproject.commands.RecipeCommand;
+import mishcma.springweb.recipeproject.commands.UnitOfMeasureCommand;
 import mishcma.springweb.recipeproject.service.IngredientService;
 import mishcma.springweb.recipeproject.service.RecipeService;
 import mishcma.springweb.recipeproject.service.UnitOfMeasureService;
@@ -69,5 +71,37 @@ public class IngredientController {
         log.debug("saved ingredient id:" + savedCommand.getId());
 
         return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newRecipe(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUnitOfMeasure(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUnitOfMeasures());
+
+        return "recipe/ingredient/ingredientform";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/delete")
+    public String deleteIngredient(@PathVariable String recipeId,
+        @PathVariable String id){
+
+        log.debug("deleting ingredient id:" + id);
+        ingredientService.deleteById(Long.valueOf(recipeId), Long.valueOf(id));
+
+        return "redirect:/recipe/" + recipeId + "/ingredients";
     }
 }
--- recipe-project/src/main/java/mishcma/springweb/recipeproject/domain/Notes.java
+++ recipe-project/src/main/java/mishcma/springweb/recipeproject/domain/Notes.java
@@ -8,8 +8,11 @@ import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
